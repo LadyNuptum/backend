35773a450c9c886e4acfd9650190f7eb6f946fa1
LadyNuptum <159070836+LadyNuptum@users.noreply.github.com>
2025-04-22 16:06:21 -0500

    Update EcommerceApplication.java

--- src/main/java/com/manoslocales/ManosLocales/Ecommerce/EcommerceApplication.java
+++ src/main/java/com/manoslocales/ManosLocales/Ecommerce/EcommerceApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("✅ BackendManos iniciado correctamente con controladores originales");
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 }
+
