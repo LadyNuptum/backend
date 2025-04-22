3758dce55c2bfa9c9282950f5b43a1778463d8b4
LadyNuptum <159070836+LadyNuptum@users.noreply.github.com>
2025-04-22 16:37:31 -0500

    Add files via upload

---
+++ src/main/java/com/manoslocales/ManosLocales/Ecommerce/EcommerceApplication.java
@@ -0,0 +1,12 @@
+package com.manoslocales.ManosLocales.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+    public static void main(String[] args) {
+        System.out.println("✅ BackendManos iniciado correctamente");
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+}
